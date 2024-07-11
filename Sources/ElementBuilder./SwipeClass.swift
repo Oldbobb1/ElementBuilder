e4bb4bef2374fb7c8d2dccac28058d812d60c710
Bobbi R <bobbi@MacBook-Air-Bobbi.local>
2024-07-11 18:45:56 +0300

    add swipe

---
+++ Sources/ElementBuilder./SwipeClass.swift
@@ -0,0 +1,41 @@
+//
+//  SwipeClass.swift
+//
+//
+//  Created by Bobbi R. on 11.07.24.
+//
+
+import UIKit
+
+public class SwipeClass {
+
+    typealias SwipeAction = () -> Void
+
+    weak var viewController: UIViewController?
+    private var leftAction: SwipeAction?, rightAction: SwipeAction?
+
+   init(viewController:UIViewController,leftAction: @escaping SwipeAction ,rightAction: @escaping SwipeAction ) {
+        self.viewController = viewController
+        self.leftAction = leftAction
+        self.rightAction = rightAction
+        setupSwipe()
+    }
+
+    public func setupSwipe() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        swipeLeft.direction = .left
+        viewController?.view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        swipeRight.direction = .right
+        viewController?.view.addGestureRecognizer(swipeRight)
+    }
+
+    @objc public func swipeLeft() {
+        leftAction?()
+    }
+
+    @objc public func swipeRight() {
+        rightAction?()
+    }
+}
