6a01af592fdcc980a4532294887dcbafc56ee1a9
Dmitry <128475324+Oldbobb1@users.noreply.github.com>
2024-07-17 12:11:17 +0300

    Update TextField.swift

--- Sources/ElementBuilder./TextField.swift
+++ Sources/ElementBuilder./TextField.swift
@@ -5,15 +5,18 @@
 import UIKit
 
 public class TextField {
-    public static func textField(placeholder: String, radius: CGFloat? = nil, font: UIFont? = nil ) -> UITextField {
+    public static func textField(placeholder: String, radius: CGFloat? = nil, font: UIFont? = nil, backgroundColor: UIColor? = nil, clipsToBounds: Bool? = nil ) -> UITextField {
         let textField = UITextField()
         textField.placeholder = placeholder
         textField.attributedPlaceholder = NSAttributedString(string: placeholder,attributes: [NSAttributedString.Key.foregroundColor: UIColor.label] )
         textField.borderStyle = .roundedRect
         textField.autocorrectionType = .yes
         textField.layer.cornerRadius = radius ?? 0
+        textField.backgroundColor = backgroundColor
         textField.font = font
+        textField.clipsToBounds = clipsToBounds ?? false
         return textField
     }
 }
+
 
