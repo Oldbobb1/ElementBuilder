f89751dd74357434100cc67643b420f0440e3b1b
Dmitry <128475324+Oldbobb1@users.noreply.github.com>
2024-07-17 11:56:11 +0300

    Update TableView.swift

--- Sources/ElementBuilder./TableView.swift
+++ Sources/ElementBuilder./TableView.swift
@@ -5,11 +5,11 @@
 import UIKit
 
 public class TableView {
-    public  static func tableView(frame: CGRect?, style: UITableView.Style?, backgroundColor: UIColor? ) -> UITableView {
+    public  static func tableView(frame: CGRect?, style: UITableView.Style?, backgroundColor: UIColor?,clipsToBounds: Bool? = nil ) -> UITableView {
         let tableView = UITableView(frame: frame ?? .null, style: style ?? .plain )
         tableView.backgroundColor = backgroundColor
         tableView.showsVerticalScrollIndicator = false
-        tableView.clipsToBounds = true
+        tableView.clipsToBounds = clipsToBounds ?? false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }
