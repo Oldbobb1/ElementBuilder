d8a74e04e27c93557aafa71b1ff71d6ce1b3c643
Dmitry <128475324+Oldbobb1@users.noreply.github.com>
2024-07-17 12:19:29 +0300

    Update Label.swift

--- Sources/ElementBuilder./Label.swift
+++ Sources/ElementBuilder./Label.swift
@@ -5,13 +5,15 @@
 import UIKit
 
 public class Label {
-    public  static func label(text: String, fontSize: CGFloat, weight: UIFont.Weight, textColor: UIColor?,textAlignment: NSTextAlignment ) -> UILabel {
+    public  static func label(text: String, fontSize: CGFloat, weight: UIFont.Weight, textColor: UIColor?,textAlignment: NSTextAlignment, numberOfLines: Int? = nil, cornerRadius: CGFloat? = nil ) -> UILabel {
         let label = UILabel()
         label.text = text
         label.textColor = textColor
         label.textAlignment = textAlignment
         label.font = UIFont.systemFont(ofSize: fontSize, weight: weight )
-        label.numberOfLines = 0
+        label.numberOfLines = numberOfLines ?? 0
+        label.layer.cornerRadius = cornerRadius ?? 0
         return label
     }
 }
+
