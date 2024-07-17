5430304d1952a1a846ad4af52b5934ffe151eb42
Dmitry <128475324+Oldbobb1@users.noreply.github.com>
2024-07-17 12:04:23 +0300

    Update StackView.swift

--- Sources/ElementBuilder./StackView.swift
+++ Sources/ElementBuilder./StackView.swift
@@ -5,9 +5,10 @@
 import UIKit
 
 public class StackView {
-    public  static func stackView() -> UIStackView {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
+    public  static func stackView(axis: NSLayoutConstraint.Axis, distribution: UIStackView.Distribution? = nil) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: [])
+        stackView.axis = axis
+        stackView.distribution = distribution ?? .fillEqually
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }
