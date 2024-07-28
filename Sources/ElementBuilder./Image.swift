bd8bdae8c82d3c699f2c8d5ee3abfb43fd941062
Bobbi R <bobbi@MacBook-Air-Bobbi.local>
2024-07-28 16:43:00 +0300

    update

--- Sources/ElementBuilder./Image.swift
+++ Sources/ElementBuilder./Image.swift
@@ -2,11 +2,11 @@ import UIKit
 
 
 public class Image {
-    public  static  func image(_ named: String,cornerRadius: CGFloat, contentMode: UIView.ContentMode, clipsToBounds: Bool? = nil ) -> UIImageView {
+    public  static  func image(_ named: String,cornerRadius: CGFloat? = nil, contentMode: UIView.ContentMode, clipsToBounds: Bool? = nil ) -> UIImageView {
         let imageView = UIImageView()
         imageView.image = UIImage(named:named)
         imageView.clipsToBounds = clipsToBounds ?? true
-        imageView.layer.cornerRadius = cornerRadius
+        imageView.layer.cornerRadius = cornerRadius ?? 0
         imageView.contentMode = contentMode
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
