5a0c72c6ff98a8e1f5debb0029698a726882f40f
Dmitry <therromanov@gmail.com>
2025-02-06 21:46:04 +0300

    update swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,14 +7,11 @@ let package = Package(
     name: "ElementBuilder",
     platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ElementBuilder",
             targets: ["ElementBuilder"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ElementBuilder"),
         .testTarget(
@@ -22,6 +19,3 @@ let package = Package(
             dependencies: ["ElementBuilder"]),
     ]
 )
-
-// swift-tools-version:5.5
-
